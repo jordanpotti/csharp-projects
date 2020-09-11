d4cb38991ef98ad9f2e145c24266b8b79c8f7d71
JP <19959240+jordanpotti@users.noreply.github.com>
2020-09-11 08:48:59 -0600

    Add files via upload

---
+++ checkin/checkin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace checkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var wb = new WebClient())
+            {
+                var url = "<Insert checkin URL here>" + "UserName=" + Environment.UserName + "&HostName=" + Environment.MachineName;
+                var data = new NameValueCollection();
+                var response = wb.DownloadString(url);
+            }
+
+        }
+}
+}
